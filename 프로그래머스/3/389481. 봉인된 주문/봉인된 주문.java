7c7924fb5e0a6f0e5d7befd13c342178a57fe016
201924611 <hu349401@gmail.com>
2025-10-25 21:58:09 +0900

    [level 3] Title: 봉인된 주문, Time: 573.02 ms, Memory: 171 MB -BaekjoonHub

---
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public String solution(long n, String[] bans) {
+
+        Arrays.sort(bans, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() > o2.length()) {
+                    return 1;
+                } else if (o1.length() < o2.length()) {
+                    return -1;
+                } else {
+                    return o1.compareTo(o2);
+                }
+            }
+        });
+
+        int ban_cnt = 0;
+
+        for(int i = 0; i < bans.length; i++) {
+            long ban_num = 0;
+            char[] c_array = bans[i].toCharArray();
+
+            for(int j = 0; j < c_array.length; j++) {
+                ban_num = ban_num + ((long)(Math.pow(26, c_array.length - 1 - j)) * (c_array[j] - 'a' + 1));
+            }
+
+            if((ban_num - ban_cnt) <= n) {
+                ban_cnt++;
+            }
+        }
+
+        n = n + ban_cnt;
+
+        String answer = "";
+
+        while(n > 0) {
+            long rem = (n-1) % 26;
+            n = (n-1) / 26;
+
+            answer = (char)('a' + rem) + answer;
+           
+        }
+
+        return answer;
+    }
+
+
+}
