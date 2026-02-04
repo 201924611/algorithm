28baf54781a81176d4e9af724c094bbbbc661de8
201924611 <hu349401@gmail.com>
2026-02-04 11:35:56 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 86.11 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,39 +1,54 @@
 import java.util.*;
-
 class Solution {
-
     public int[] solution(long[] numbers) {
         int[] answer = new int[numbers.length];
-
-        for (int i = 0; i < numbers.length; i++) {
+        // 1. 수를 이진수로 표현하고 배열에 넣을 떄 루트 노드가 존재하면서 이진트리가 되면 성공
+        // 2. 문자열은 2제곱 - 1만큼의 크기가 되어야함
+        for(int i = 0; i<numbers.length; i++){
             String str = Long.toBinaryString(numbers[i]);
-
+            // 크기 구하기
             int len = str.length();
             int size = 1;
-            while (size - 1 < len) size <<= 1;
-
+            while (size - 1 < len) {
+                size *= 2;
+            }
             str = "0".repeat((size - 1) - len) + str;
-
-            answer[i] = isValid(str, 0, str.length() - 1) ? 1 : 0;
+            // 이진트리 되는지 체크용
+            boolean state = false;
+            int min = 0;
+            int max = size - 2;
+            int med = (min + max) /2;
+            if(str.charAt(med) == '0'){
+                continue;
+            }
+            // 루트
+            Queue<Integer> que = new LinkedList<>();
+            que.add(min);
+            que.add(max);
+            while(!que.isEmpty()){
+                int tempMin = que.poll();
+                int tempMax = que.poll();
+                int tempMed = (tempMin + tempMax) /2;
+                if( str.charAt(tempMed) == '1' ) {
+                    if(tempMin < tempMed - 1){
+                        que.add(tempMin);
+                        que.add(tempMed-1);
+                    }
+                    if(tempMax > tempMed + 1){
+                        que.add(tempMed + 1);
+                        que.add(tempMax);
+                    }
+                }else{
+                    for(int j = tempMin; j<= tempMax; j++){
+                        if( str.charAt(j) == '1') state = true;
+                        if(state) break;
+                    }
+                    if(state) break;
+                }
+            }
+            if(state) answer[i] = 0;
+            else answer[i] = 1;
         }
         return answer;
-    }
-
-    private boolean isValid(String str, int left, int right) {
-        if (left > right) return true;
-
-        int mid = (left + right) / 2;
-
-        if (str.charAt(mid) == '0') {
-            // 부모가 0이면, 이 구간에 1이 있으면 안 됨
-            for (int i = left; i <= right; i++) {
-                if (str.charAt(i) == '1') return false;
-            }
-            return true;
-        }
-
-        // 부모가 1이면 자식 검사
-        return isValid(str, left, mid - 1)
-            && isValid(str, mid + 1, right);
     }
 }
